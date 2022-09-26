cbbb9c356c37ee92b9ffe635b61067a0ecfc3bb7
cheeter_Lee <johnson991210@gmail.com>
2022-09-26 09:43:11 +0800

    fix bug

--- src/componenets/Homepage.jsx
+++ src/componenets/Homepage.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 import { useGetCryptosQuery } from '../services/cryptoApi'
 import Cryptocurrencies from './Cryptocurrencies'
 import News from './News'
+import Loader from './Loader'
 
 const { Title } = Typography
 
@@ -14,7 +15,7 @@ const Homepage = () => {
   const globalStats = data?.data?.stats
 
   if (isFetching) {
-    return 'Loading...'
+    return <Loader />
   }
 
   return (
